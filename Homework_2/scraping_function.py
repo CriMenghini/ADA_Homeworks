80da7fd9e9ca76256d290c47f0494ae7096827d8
Cristina Menghini <cricri.menghini@gmail.com>
2016-10-17 22:27:18 +0200

    [DONE] final version

--- Homework_2/scraping_function.py
+++ Homework_2/scraping_function.py
@@ -5,17 +5,20 @@ Created on Thu Oct 13 21:03:22 2016
 @author: cristinamenghini
 """
 
-import pandas as pd
+import os
+import glob
 import requests
+import functools
+import pandas as pd
 from bs4 import BeautifulSoup
 
 def create_parameter_dict(list_par, html_file):
     """This function returns a dictionary of that form
     
-                {'parameter_1' : {'name_1' : value_1, ..., 'name_n' : value_n},
-                 'parameter_2' : {'name_1' : value_1, ..., 'name_n' : value_n},
-                  ...........,
-                 'parameter_k' : {'name_1' : value_1, ..., 'name_n' : value_n}}
+                {'parameter_1' : {'name_1' : value_1, ..., 'name_p1' : value_p1},
+                  'parameter_2' : {'name_1' : value_1, ..., 'name_p2' : value_p2},
+                   ...........,
+                  'parameter_k' : {'name_1' : value_1, ..., 'name_pk' : value_pk}}
                   
         It takes as input:
         @list_par = the list of parameters (includes : Academic unit, Academic period, 
@@ -34,6 +37,62 @@ def create_parameter_dict(list_par, html_file):
         format_field[f] = {i.string : i['value'] for i in html_file.input(attrs = {'name': f})[0] if i.string != None}
     
     return (format_field)
+
+
+def compose_url(dic_par, semester, format_ = 'html', department = 'Informatique'):
+    '''
+        This function returns the entire url for given arguments
+        @dic_par is dictionary returned by function create_parameter_dict
+        @semester is the name of the semester
+        @format_ is the format we use to download the data
+        @department is the name of department
+    '''
+    base_url = 'http://isa.epfl.ch/imoniteur_ISAP/!GEDPUBLICREPORTS.html?'
+    
+    # html arguments taken from Postman
+    ww_x_GPS = 'ww_x_GPS=-1'
+    ww_i_reportModel = 'ww_i_reportModel=133685247'
+    ww_i_reportModelXsl = 'ww_i_reportModelXsl=' + dic_par['ww_i_reportModelXsl'][format_]
+    ww_x_UNITE_ACAD = 'ww_x_UNITE_ACAD=' + dic_par['ww_x_UNITE_ACAD'][department]
+    ww_x_PERIODE_PEDAGO = 'ww_x_PERIODE_PEDAGO=' + dic_par['ww_x_PERIODE_PEDAGO'][semester]
+    
+    list_ = [ww_i_reportModel, ww_i_reportModelXsl, ww_x_UNITE_ACAD, ww_x_PERIODE_PEDAGO]
+    
+    # We are folding the list from the left with html arguments to concatenate them using conjunction operator
+    foldleft = functools.reduce(lambda x, y: x + '&' + y, list_, base_url + ww_x_GPS)
+    
+    return foldleft
+
+
+def fetch_files(data_name, urls):
+    '''
+        This function downloads the data into the directory specified by @data_name
+        @data_name is the name of the directory we want the data to be downloaded to
+    '''
+    
+    # Creating an empty directory if it doesn't exist
+    try:
+        os.mkdir(data_name)
+    except:
+        pass
+
+    # Changing the permissions in order to avoid problems
+    os.chmod(data_name, 0o777)
+
+    # Remove old files from this directory
+    files = glob.glob(data_name + '/*')
+    for f in files:
+        os.remove(f)
+
+    # Downloading the data into the @data_name directory
+    k = 0
+    for url in urls:
+        k += 1
+        print ( k, ' request sent.')
+        save_data(url, data_name + '/') # funtion defined in separate file
+        print ('Data collected from the ', k, ' request.')
+        print ('*****'*10)
+    print('Done')
     
 
 def fun_rec(indexes, df_init, df_new = []):
